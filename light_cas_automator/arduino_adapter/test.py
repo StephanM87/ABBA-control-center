d41e1a4b4537ecf6df4498ca7adedb2e21bd8eb7
s.malzacher <s.malzacher@fz-juelich.de>
2022-03-23 16:56:57 +0100

    feat: correct pin added to LED stripes

--- light_cas_automator/arduino_adapter/test.py
+++ light_cas_automator/arduino_adapter/test.py
@@ -1,19 +1,18 @@
 import pyfirmata
 import time
 if __name__ == '__main__':
-    board = pyfirmata.Arduino('COM5')
-    LED_links = board.get_pin('d:2:o')
-    LED_rechts = board.get_pin('d:3:o')
+    board = pyfirmata.Arduino('COM3')
+    LED_links = board.get_pin('d:6:o')
+    #LED_rechts = board.get_pin('d:3:o')
 
     def LED_start():
 
         while True:
+            print(1)
             LED_links.write(1)
-            LED_rechts.write(0)
             time.sleep(1.0)
 
             LED_links.write(0)
-            LED_rechts.write(1)
             time.sleep(1.0)
 
 LED_start()
