107923bfc4dc4d4cce42bd5f2d56afc8c51a9fe8
s.malzacher <s.malzacher@fz-juelich.de>
2022-06-23 15:48:17 +0200

    feat: pyfirmata actualised

--- light_cas_automator/arduino_adapter/control_arduino.py
+++ light_cas_automator/arduino_adapter/control_arduino.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_restx import Namespace, Resource, Api, fields, abort
 from werkzeug.exceptions import BadRequest, HTTPException
 import os
+import pyfirmata
 
 
 from light_cas_automator.arduino_adapter.control_panel import ControlPanel
+
+board = pyfirmata.Arduino(self.COM)
 
 namespace = Namespace("api/ot", description="Route whicht creates Enzymeml documents and returns them in form of omex archives")
 
@@ -31,7 +34,6 @@ class PumpControl(Resource):
         control = ControlPanel("COM3")
         control.start_pump_foward()
         return "Pumpe läuft"
-
 @namespace.route("/stop_pump")
 class PumpControl(Resource):
     @namespace.doc()
