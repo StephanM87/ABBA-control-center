f262324681378227dcf6c700c63c1de7e91f4ebf
Stephan Malzacher <s.malzacher@web.de>
2022-07-27 01:24:57 +0200

    hbjku

--- light_cas_automator/arduino_adapter/control_panel.py
+++ light_cas_automator/arduino_adapter/control_panel.py
@@ -1,56 +1,24 @@
-import pyfirmata
 
+from light_cas_automator.arduino_adapter.spinsolve_message_reader import SpinsolveMessageReader
 
 class ControlPanel:
-    pass
-'''
-    def __init__(self, board):
-
-        self.COM = board
-        self.board = board
     
-    def build_connection(self):
-        try:
-            board = pyfirmata.Arduino("COM3")
-            return board
-        except Exception as Err:
-            print(Err, "already initialised")
-            raise
-    
-    def start_led(self):
-        board = self.build_connection()
-        LED_links = board.get_pin('d:6:o')
-        LED_links.write(1)
-        board.exit()
+    def check_measurement(self):
 
-    def start_led_init(self):
-            #board = self.build_connection()
-            LED_links = board.get_pin('d:6:o')
-            LED_links.write(1)
-            #board.exit()
+        def __init__(self, socket, command):
+            self.socket = socket
+            self.command = command
 
-'''
-'''
-    def stop_led(self):
-        #board = self.build_connection()
-        LED_links = board.get_pin('d:6:o')
-        LED_links.write(0)
-        #board.exit()
-    
-    def start_pump_foward(self):
-        print("hallo pump")
-        board = self.build_connection()
-        p_pump_pwm = board.get_pin('d:9:p')
-        p_pump_pwm.write(0.7)
-        p_pump_direction = board.get_pin('d:3:o')
-        p_pump_direction.write(0)
-        board.exit()
+        def get_messages(self):
+            chunk = self.socket.recv(8129)
+            payload_stat = SpinsolveMessageReader(self.socket, chunk, self.command)
+            messages = payload_stat.define_cases()
+            return messages
 
-    def stop_pump(self):
-        board = self.build_connection()
-        p_pump_on_off = board.get_pin('d:2:o')
-        p_pump_on_off.write(1)
-        board.exit()
+        def get_status(self):
 
-'''
+            messages = self.get_messages()
+            
+
+
 
