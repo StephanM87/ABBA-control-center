370acb2c46ab3bb7666f60c072d630a217e85b59
StephanM87 <SMalzacher@web.de>
2023-07-13 12:35:22 +0200

    started a file with the aim to develop a test for the system

--- light_cas_automator/arduino_adapter/test.py
+++ light_cas_automator/arduino_adapter/test.py
@@ -1,3 +1,20 @@
+'''
+
+This Method aims to control the hardware communication used in the LightCas cascade.
+One of the biggest challenges in this project is that a test possibility is missing, able to mimic the reaction
+
+In this method the following functions are aimed to be tested:
+
+1. Import and anlysis of the measured values by the NMR
+2. Control of the connected hardware:
+   - Measurement Pump
+   - Dosage pump 1
+   - Dosage pump 2
+   - LED strip
+
+'''
+
+
 import pyfirmata
 import time
 if __name__ == '__main__':
